03ad0dde43312fa32878d3d44d1349e451eeb8da
Рифат Хасанов <Hidex-morfeyka@mail.ru>
2022-11-22 23:01:31 +0300

    Incorporate a Mongodb and implement CRUD operations

--- models/events.py
+++ models/events.py
@@ -1,15 +1,36 @@
+from beanie import Document
 from pydantic import BaseModel
+from typing import Optional, List
 
-from typing import List
 
-
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
     tags: List[str]
     location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "How to make rasengan",
+                "image": "https://jut.su/rasengan.png",
+                "description": "Learning jutsu from Naruto anime",
+                "tags": ["Naruto", "Konoha", "Ninja"],
+                "location": "Discord"
+            }
+        }
+
+    class Settings:
+        name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         schema_extra = {
--- models/users.py
+++ models/users.py
@@ -1,14 +1,18 @@
+from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 from typing import Optional, List
 
 from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     username: str
     password: str
-    events: Optional[List[Event]]
+    events: Optional[List[Link[Event]]]
+
+    class Settings:
+        name = "users"
 
     class Config:
         schema_extra = {
