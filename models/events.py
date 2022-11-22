8c210d456faf58954ec33e58706f547cd6bb1e81
Рифат Хасанов <Hidex-morfeyka@mail.ru>
2022-11-22 21:30:21 +0300

    Incorporate a SQL database and implement CRUD operations

--- models/events.py
+++ models/events.py
@@ -1,15 +1,35 @@
-from pydantic import BaseModel
+from typing import Optional, List
 
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
 
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "title": "How to make rasengan",
+                "image": "https://jut.su/rasengan.png",
+                "description": "Learning jutsu from Naruto anime",
+                "tags": ["Naruto", "Konoha", "Ninja"],
+                "location": "Discord"
+            }
+        }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         schema_extra = {
@@ -21,3 +41,4 @@ class Event(BaseModel):
                 "location": "Discord"
             }
         }
+
